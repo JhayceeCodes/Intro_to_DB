8169e4ca837b78f7d89d6f30aaf0c8db2e4f8154
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-12-14 20:23:20 +0100

    fix: remove extra space for book_id in Order_Details table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -37,7 +37,7 @@ CREATE TABLE Orders(
 CREATE TABLE Order_Details(
     orderdetailid INT UNSIGNED AUTO_INCREMENT,
     order_id INT UNSIGNED,
-    book_id  INT,
+    book_id INT UNSIGNED,
     quantity DOUBLE,
     PRIMARY KEY (orderdetailid),
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
