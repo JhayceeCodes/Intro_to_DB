ad5aa372fe99faa137f26ba0a10af4d453f70882
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-12-14 21:29:33 +0100

    feat: add task_2.sql tp create tables for alx book store

---
+++ task_2.sql
@@ -0,0 +1,42 @@
+CREATE TABLE IF NOT EXISTS Authors(
+    author_id INT UNSIGNED AUTO_INCREMENT,
+    author_name VARCHAR(215),
+    PRIMARY KEY (author_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS Books(
+    books_id INT UNSIGNED AUTO_INCREMENT,
+    title VARCHAR(130),
+    author_id INT UNSIGNED,
+    price DOUBLE,
+    publication_date DATE,
+    PRIMARY KEY (books_id),
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id INT UNSIGNED AUTO_INCREMENT,
+    customer_name VARCHAR(215),
+    email VARCHAR(215) NOT NULL,
+    address TEXT,
+    PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS Orders(
+    orders_id INT UNSIGNED AUTO_INCREMENT,
+    customer_id INT UNSIGNED,
+    order_date DATE,
+    PRIMARY KEY (orders_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS Order_Details(
+    orderdetailid INT UNSIGNED AUTO_INCREMENT,
+    order_id INT UNSIGNED,
+    book_id INT UNSIGNED,
+    quantity DOUBLE,
+    PRIMARY KEY (orderdetailid),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
